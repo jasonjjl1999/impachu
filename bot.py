e29d8615d955b8ad9e94fe1cc48b2bd19aab0b09
jasonjjl1999 <jasonjjl1999@gmail.com>
2021-01-01 21:03:25 -0800

    Fixed help messages

--- bot.py
+++ bot.py
@@ -10,19 +10,26 @@ import impachu
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Initialize bot
 bot = commands.Bot(
     command_prefix='!',
-    description='Boungiorno! I am Impachu.')
+    description='Boungiorno! I am Impachu.',
+    help_command=help_command)
+
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
         
     # Set Activity
-    activity = discord.Game(name="!helpachu", type=3)
+    activity = discord.Game(name='!help', type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     return
 
+@bot.command(name='test')
+async def help(ctx):
+    await bot.send_help()
 
 @bot.command(
     name='impachu', 
