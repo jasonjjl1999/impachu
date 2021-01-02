0c3f3e7f77e88bbaf3c5ec54d1f2f0045e2324e1
jasonjjl1999 <jasonjjl1999@gmail.com>
2021-01-02 02:16:19 -0800

    Cleaned up classes

--- templates.py
+++ templates.py
@@ -34,8 +34,7 @@ class ImpactMeme(Meme):
         """
         super().__init__()
         self.image_url = ''
-        self.top_text = ''
-        self.bottom_text = ''
+        self.draw = None
         self.font_type = 'fonts/impact.ttf'
 
         return
@@ -47,8 +46,8 @@ class ImpactMeme(Meme):
         """
         self.image_url = url
         response = requests.get(self.image_url)
-        self.composition = BytesIO(response.content)
-        self.composition = Image.open(self.composition)
+        self.composition = Image.open(BytesIO(response.content))
+        self.draw = ImageDraw.Draw(self.composition)
 
         return
 
@@ -62,57 +61,52 @@ class ImpactMeme(Meme):
         font = ImageFont.truetype(self.font_type, font_size)
 
         # Shrink size if text larger than width of image
-        draw = ImageDraw.Draw(self.composition)
-        text_width, _ = draw.textsize(text, font=font)
+        text_width, _ = self.draw.textsize(text, font=font)
         image_width, _ = self.composition.size
         while text_width >= image_width:
             font_size -= 5
             font = ImageFont.truetype(self.font_type, font_size)
-            text_width, _ = draw.textsize(text, font=font)
+            text_width, _ = self.draw.textsize(text, font=font)
 
         return font
 
 
-    def set_toptext(self, text):
-        self.top_text = text
-        draw = ImageDraw.Draw(self.composition)
-        font = self.__get_font(self.top_text)
+    def set_toptext(self, top_text):
+        font = self.__get_font(top_text)
 
         # Center text
-        text_width, _ = draw.textsize(self.top_text, font=font)
+        text_width, _ = self.draw.textsize(top_text, font=font)
         image_width, _ = self.composition.size
         x = (image_width - text_width) / 2
         y = 10
 
-        self.__draw_outline_text(draw=draw, x=x, y=y, text=self.top_text, font=font) 
+        self.__draw_outline_text(x=x, y=y, text=top_text, font=font) 
 
         return
 
 
-    def set_bottomtext(self, text):
-        self.bottom_text = text
-        draw = ImageDraw.Draw(self.composition)
-        font = self.__get_font(self.bottom_text)
+    def set_bottomtext(self, bottom_text):
+        font = self.__get_font(bottom_text)
 
         # Center text
-        text_width, text_height = draw.textsize(self.bottom_text, font=font)
+        text_width, text_height = self.draw.textsize(bottom_text, font=font)
         image_width, image_height = self.composition.size
         x = (image_width - text_width) / 2
         y = image_height - text_height - 10
 
-        self.__draw_outline_text(draw=draw, x=x, y=y, text=self.bottom_text, font=font)
+        self.__draw_outline_text(x=x, y=y, text=bottom_text, font=font)
 
         return
 
 
-    def __draw_outline_text(self, draw, x, y, text, font):
+    def __draw_outline_text(self, x, y, text, font):
         """
         Add signature outline to Impact Font
         """
         # Determine black border based on font height
         stroke_width = int(font.getsize(text)[1] / 15)
 
-        draw.text((x, y), text, fill=(0, 0, 0), font=font, stroke_width=stroke_width)
-        draw.text((x, y), text, fill=(255, 255, 255), font=font)  
+        self.draw.text((x, y), text, fill=(0, 0, 0), font=font, stroke_width=stroke_width)
+        self.draw.text((x, y), text, fill=(255, 255, 255), font=font)  
 
         return
