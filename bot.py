4092d6cf9ae7078e047b865526fe20f975349c4a
jasonjjl1999 <jasonjjl1999@gmail.com>
2021-01-01 19:45:26 -0800

    Added help function

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 from io import BytesIO
 import discord
+import shlex
 from dotenv import load_dotenv
 
 import impachu
@@ -13,6 +14,7 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
 @client.event 
 async def on_message(message):
 
@@ -20,33 +22,52 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # Respond to command only
+    if message.content.startswith('--helpachu'):
+        await help_command(message)
+
     if message.content.startswith('--impact'):
+        await impact_command(message)
 
-        top_text = ''
-        bottom_text = ''
-        url_valid = False
 
-        for param in message.content.split():
-            if param.startswith('url'):
-                url = param.split('=')[1]
-                url_valid = True
-            if param.startswith('top'):
-                top_text = param.split('=')[1]
-            if param.startswith('bottom'):
-                bottom_text = param.split('=')[1]
+async def help_command(message):
+    await message.channel.send("""
+    Buongiorno! I am Impachu.
 
-        if not url_valid:
-            print ("Image URL missing")
-            return False
+    **• helpachu**: Help
+        Usage: `--helpachu`\n
+    **• impact**: Impact meme maker
+        Usage: `--impact <URL> "<Top Text>" "<Bottom Text>"`\n""")
 
-        edited_meme = impachu.make_impact_meme(image_url=url, top_text=top_text, bottom_text=bottom_text)
-        result_image = edited_meme.get_result()
+    return
 
-        await message.channel.send("Happy New Year!")
-        arr = BytesIO()
-        result_image.save(arr, format='PNG')
-        arr.seek(0)
-        await message.channel.send(file=discord.File(fp=arr, filename='meme.png'))
+async def impact_command(message):
+    """
+    Bot Response for '--impact' command
+    format:
+        --impact <URL> "<Top Text>" "<Bottom Text>"
+    """
+
+    top_text = ''
+    bottom_text = ''
+    url_valid = False
+
+    # Parse Parameters
+    params = shlex.split(message.content)
+    url = params[1]
+    top_text = params[2]
+    if len(params) >= 4:
+        bottom_text = params[3]
+
+    edited_meme = impachu.make_impact_meme(image_url=url, top_text=top_text, bottom_text=bottom_text)
+    result_image = edited_meme.get_result()
+
+    await message.channel.send("Happy New Year!")
+    arr = BytesIO()
+    result_image.save(arr, format='PNG')
+    arr.seek(0)
+    await message.channel.send(file=discord.File(fp=arr, filename='meme.png'))
+
+    return
+
 
 client.run(TOKEN)
