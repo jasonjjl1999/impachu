6a8ef1d915b88e320e1c353d2aee80343546028f
jasonjjl1999 <jasonjjl1999@gmail.com>
2021-01-01 20:41:59 -0800

    Changed bot to use built int command module

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 from io import BytesIO
 import discord
+from discord.ext import commands
 import shlex
 from dotenv import load_dotenv
 
@@ -8,66 +9,40 @@ import impachu
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
 
-@client.event
+bot = commands.Bot(
+    command_prefix='!',
+    description='Boungiorno! I am Impachu.')
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-
-@client.event 
-async def on_message(message):
-
-    # Prevent bot calling itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('--helpachu'):
-        await help_command(message)
-
-    if message.content.startswith('--impact'):
-        await impact_command(message)
-
-
-async def help_command(message):
-    await message.channel.send("""
-    Buongiorno! I am Impachu.
-
-    **• helpachu**: Help
-        Usage: `--helpachu`\n
-    **• impact**: Impact meme maker
-        Usage: `--impact <URL> "<Top Text>" "<Bottom Text>"`\n""")
-
+    print(f'{bot.user} has connected to Discord!')
+        
+    # Set Activity
+    activity = discord.Game(name="!helpachu", type=3)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
     return
 
-async def impact_command(message):
+
+@bot.command(
+    name='impachu', 
+    help='Adds impact font to any image to create a meme')
+async def impact_command(ctx, url, top_text, bottom_text=''):
     """
     Bot Response for '--impact' command
     format:
         --impact <URL> "<Top Text>" "<Bottom Text>"
     """
 
-    top_text = ''
-    bottom_text = ''
-    url_valid = False
-
-    # Parse Parameters
-    params = shlex.split(message.content)
-    url = params[1]
-    top_text = params[2]
-    if len(params) >= 4:
-        bottom_text = params[3]
-
     edited_meme = impachu.make_impact_meme(image_url=url, top_text=top_text, bottom_text=bottom_text)
     result_image = edited_meme.get_result()
 
-    await message.channel.send("Happy New Year!")
+    await ctx.send("Happy New Year!")
     arr = BytesIO()
     result_image.save(arr, format='PNG')
     arr.seek(0)
-    await message.channel.send(file=discord.File(fp=arr, filename='meme.png'))
+    await ctx.send(file=discord.File(fp=arr, filename='meme.png'))
 
     return
 
-
-client.run(TOKEN)
+bot.run(TOKEN, bot=True)
