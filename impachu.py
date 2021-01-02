36a8d3fd1eaae22f85016e74b867e2e20b828eb2
jasonjjl1999 <jasonjjl1999@gmail.com>
2021-01-01 17:37:34 -0800

    Added functions to create Impact memes

--- impachu.py
+++ impachu.py
@@ -1,6 +1,21 @@
 import templates
 
-my_meme = templates.ImpactMeme()
-my_meme.set_image('https://images.theconversation.com/files/177834/original/file-20170712-14488-19lw3sc.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop')
+def main():
 
-my_meme.render_meme()
+    make_impact_meme()
+
+
+def make_impact_meme(image_url, font_size, top_text, bottom_text):
+
+    my_meme = templates.ImpactMeme()
+    my_meme.set_image(image_url)
+    my_meme.set_fontsize(font_size)
+    my_meme.set_toptext(top_text)
+    my_meme.set_bottomtext(bottom_text)
+    my_meme.render_meme()
+
+    return
+
+if __name__ == '__main__':
+
+    main()
--- templates.py
+++ templates.py
@@ -9,14 +9,17 @@ class Meme:
     The basic meme object
     """
     def __init__(self):
-        self.image = None
+        self.composition = None
+
+        return
 
     """
     Displays the meme to see
     """
     def render_meme(self):
-        meme = Image.open(self.image)
-        meme.show()
+        self.composition.show()
+
+        return
 
 class ImpactMeme(Meme):
     def __init__(self):
@@ -27,6 +30,11 @@ class ImpactMeme(Meme):
         self.image_url = ''
         self.top_text = ''
         self.bottom_text = ''
+        self.font_size = 100
+        self.font_type = 'fonts/impact.ttf'
+        self.font = ImageFont.truetype(self.font_type, self.font_size)
+
+        return
 
     def set_image(self, url):
         """
@@ -34,5 +42,53 @@ class ImpactMeme(Meme):
         """
         self.image_url = url
         response = requests.get(self.image_url)
-        self.image = BytesIO(response.content)
+        self.composition = BytesIO(response.content)
+        self.composition = Image.open(self.composition)
 
+        return
+
+    def set_fontsize(self, size):
+        self.font_size = size
+        self.font = ImageFont.truetype(self.font_type, self.font_size)
+
+        return
+
+    def set_toptext(self, text):
+        self.top_text = text
+        draw = ImageDraw.Draw(self.composition)
+
+        # Center text
+        text_width, _ = draw.textsize(self.top_text, font=self.font)
+        image_width, _ = self.composition.size
+        x = (image_width - text_width) / 2
+        y = 10
+
+        self.__draw_outline_text(draw=draw, x=x, y=y, text=self.top_text, font=self.font) 
+
+        return
+
+    def set_bottomtext(self, text):
+        self.bottom_text = text
+        draw = ImageDraw.Draw(self.composition)
+
+        # Center text
+        text_width, text_height = draw.textsize(self.bottom_text, font=self.font)
+        image_width, image_height = self.composition.size
+        x = (image_width - text_width) / 2
+        y = image_height - text_height - 10
+
+        self.__draw_outline_text(draw=draw, x=x, y=y, text=self.bottom_text, font=self.font)
+
+        return
+
+    def __draw_outline_text(self, draw, x, y, text, font):
+        """
+        Add signature outline to Impact Font
+        """
+        draw.text((x-1, y), text, fill=(0, 0, 0), font=font)
+        draw.text((x+1, y), text, fill=(0, 0, 0), font=font)
+        draw.text((x, y-1), text, fill=(0, 0, 0), font=font)
+        draw.text((x, y+1), text, fill=(0, 0, 0), font=font)
+        draw.text((x, y), text, fill=(255, 255, 255), font=font)  
+
+        return
