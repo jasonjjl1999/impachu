9b79b6140e9d1759a3f60b752c90117cdf6878e8
jasonjjl1999 <jasonjjl1999@gmail.com>
2021-06-30 00:33:43 -0700

    Fixed typo causing duplicate categories

--- impachu/models/commands.py
+++ impachu/models/commands.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from impachu.models.templates import GANsNRoses
 
 
-class Models(commands.Cog, name='Memes'):
+class Models(commands.Cog, name='Models'):
     """
     Commands for using various types of AI-models
     """
